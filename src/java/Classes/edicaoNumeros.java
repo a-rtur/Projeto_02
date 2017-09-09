91dd08bbad9b3adc903c63c91e8b623dcb02cc7f
Artur <Artur@Artur-PC>
2017-09-09 13:44:59 -0300

    Adicionado classe com métodos para formatação monetária e aceitar vírgulas.

---
+++ src/java/Classes/edicaoNumeros.java
@@ -0,0 +1,25 @@
+package Classes;
+
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
+/**
+ *
+ * @author Artur
+ */
+public class edicaoNumeros {
+    public static String formatarVariavel(double valor) {
+        DecimalFormat df;
+        df = new DecimalFormat("##,###,###,##0.00", new DecimalFormatSymbols(new Locale("pt", "BR")));
+        df.setMinimumFractionDigits(2);
+        df.setParseBigDecimal(true);
+        return df.format(valor);
+    }
+
+    public static double aceitaVirgula(String valor) {
+        double retorno;
+        valor = valor.replace(',', '.'); 
+        retorno = Double.parseDouble(valor);
+        return retorno;
+    }
+}
