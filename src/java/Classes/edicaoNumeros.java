ea25f0fba086af98665dd019dcd5503cf0075088
Artur <Artur@Artur-PC>
2017-09-12 13:18:42 -0300

    Método formatarVariavel retornará 0 ao invés de -0.0 caso valor seja muito pequeno

--- src/java/Classes/edicaoNumeros.java
+++ src/java/Classes/edicaoNumeros.java
@@ -9,11 +9,16 @@ import java.util.Locale;
  */
 public class edicaoNumeros {
     public static String formatarVariavel(double valor) {
-        DecimalFormat df;
-        df = new DecimalFormat("##,###,###,##0.00", new DecimalFormatSymbols(new Locale("pt", "BR")));
-        df.setMinimumFractionDigits(2);
-        df.setParseBigDecimal(true);
-        return df.format(valor);
+            DecimalFormat df;
+            df = new DecimalFormat("##,###,###,##0.00", new DecimalFormatSymbols(new Locale("pt", "BR")));
+            df.setMinimumFractionDigits(2);
+            df.setParseBigDecimal(true);
+            if (valor < 1) {
+                return df.format(0);
+            }
+            else {
+                return df.format(valor);
+            }
     }
 
     public static double aceitaVirgula(String valor) {
